2cc01b43d3b48d45088da7ab0c4c9c788f36503a
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-13 08:34:59 -0700

    complete the nickname and set nickname methods

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,19 @@
 class Player
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :nickname
 
   def initialize(name, monthly_cost, contract_length)
     @first_name = name.split.first
     @last_name = name.split.last
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @nickname = nil
+  end
+
+  def total_cost
+    @contract_length * @monthly_cost
+  end
+
+  def set_nickname!(new_nickname)
+    @nickname = new_nickname
   end
 end
