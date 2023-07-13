b932b7c9ead80696204889b3804abf8db54a0abc
Robert deLaguna <rjdelaguna@yahoo.com>
2023-07-13 08:27:50 -0700

    Create first and last name attributes

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,8 @@
 class Player
+  attr_reader :first_name, :last_name
 
   def initialize(name, monthly_cost, contract_length)
+    @first_name = name.split.first
+    @last_name = name.split.last
   end
 end
